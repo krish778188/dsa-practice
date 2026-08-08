e65e86d7bf68ffc78fb945b7c7dadea48bc083f3
krish778188 <krishraj453@gmail.com>
2026-08-08 16:53:17 +0530

    Time: 0 ms (100%), Space: 10.5 MB (71.97%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,27 +1,24 @@
 class Solution {
     unsigned long long factorial(int n) {
-        if (n < 0)
-            return 0;
+        if (n < 0) return 0;
         unsigned long long result = 1;
         for (int i = 1; i <= n; ++i) {
             result *= i;
         }
         return result;
     }
-
 public:
     vector<int> perm(vector<int>& nums, int itr) {
-        if(itr == factorial(nums.size()))
-            return nums;
+        if(itr == factorial(nums.size())) return nums;
         bool has_next = std::next_permutation(nums.begin(), nums.end());
-        if(has_next) return nums;
+        if (has_next) return nums;
         return nums;
     }
     vector<vector<int>> permute(vector<int>& nums) {
         int n = nums.size();
         int itr = factorial(n);
         vector<vector<int>> ans;
-        while (itr > 0) {
+        while(itr > 0) {
             ans.push_back(perm(nums, itr--));
         }
         return ans;
